e18fcad73d42ff0891742722b1e9068828cd985e
Nicolas Grekas <nicolas.grekas@gmail.com>
2024-09-01 10:02:27 +0200

    -

--- assets/app.js
+++ assets/app.js
@@ -8,31 +8,3 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
-
-document.addEventListener('turbo:submit-start', (event) => {
-    const csrfField = event.detail.formSubmission.formElement.querySelector('input[type="hidden"][data-csrf-protection]');
-
-    if (csrfField) {
-        event.detail.formSubmission.fetchRequest.headers[csrfField.getAttribute('data-csrf-protection')] = csrfField.value;
-    }
-});
-
-document.addEventListener('submit', (event) => {
-    const csrfField = event.target.querySelector('input[type="hidden"][data-csrf-protection]');
-
-    if (!csrfField) {
-        return;
-    }
-
-    const csrfHeader = csrfField.getAttribute('data-csrf-protection');
-
-    if (!/^[-a-zA-Z0-9_]+$/.test(csrfHeader)) {
-        return
-    }
-
-    const csrfToken = btoa(String.fromCharCode.apply(null, (window.crypto || window.msCrypto).getRandomValues(new Uint8Array(18))));
-    csrfField.value = csrfToken;
-    const cookie = csrfHeader + '=' + csrfToken + '; path=/; samesite=strict';
-
-    document.cookie = window.location.protocol === 'https:' ? '__Host-' + cookie + '; secure' : cookie;
-});
